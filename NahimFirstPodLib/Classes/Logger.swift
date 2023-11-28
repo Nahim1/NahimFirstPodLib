c707a0639f0ed2d1e45f59619ee22c0cd5e0683d
Nahim1 <nahim.makhani7786@gmail.com>
2023-11-28 16:12:34 +0530

    initial commit

---
+++ NahimFirstPodLib/Classes/Logger.swift
@@ -0,0 +1,19 @@
+//
+//  Logger.swift
+//  MyPodSDK
+//
+//  Created by APPLE on 28/11/23.
+//
+
+import Foundation
+public class Logger {
+    public init() {}
+    
+    public func printLog() {
+        print("print log")
+    }
+    
+    func privateMethhod() {
+        print("privateMethhod called")
+    }
+}
